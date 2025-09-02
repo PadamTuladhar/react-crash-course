5f2c19e953a2d8c3ee31e8a39f6302712e93f1bd
PadamTuladhar <tuladharpadam@gmail.com>
2025-09-01 20:18:57 -0400

    added lazy loading

--- react-crash-course/src/App.tsx
+++ react-crash-course/src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
-import Favourites from "./Pages/Favourites";
 import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import { MovieProvider } from "./context/MovieContext";
+import { lazy, Suspense } from "react";
 
 function App() {
+  const FavoriteComponent = lazy(() => import("./Pages/Favourites"));
+  const HomeComponent = lazy(() => import("./Pages/Home"));
   return (
     <MovieProvider>
       <Navbar />
       <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favourites />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomeComponent />} />
+            <Route path="/favorites" element={<FavoriteComponent />} />
+          </Routes>
+        </Suspense>
       </main>
     </MovieProvider>
   );
